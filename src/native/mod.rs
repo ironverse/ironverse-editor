b7ea98206c5abc90bfe44d2f5506820348959d9f
Nico <colete_nico@yahoo.com>
2023-05-20 14:09:32 +0800

    Fix mouse grab on linux

--- src/native/mod.rs
+++ src/native/mod.rs
@@ -1,12 +1,12 @@
 use bevy::{prelude::*, input::mouse::MouseButtonInput, window::CursorGrabMode};
-use crate::input::MouseInput;
+use crate::{input::MouseInput, data::CursorState};
 
 pub struct CustomPlugin;
 impl Plugin for CustomPlugin {
   fn build(&self, app: &mut App) {
     app
       .add_system(update)
-      // .add_system(grab_mouse)
+      .add_system(grab_mouse)
       ;
   }
 }
@@ -25,15 +25,18 @@ fn grab_mouse(
   mut windows: Query<&mut Window>,
   mouse: Res<Input<MouseButton>>,
   key: Res<Input<KeyCode>>,
+  mut cursor_state: ResMut<NextState<CursorState>>,
 ) {
-  // let mut window = windows.single_mut();
-  // if mouse.just_pressed(MouseButton::Left) {
-  //   window.cursor.visible = false;
-  //   window.cursor.grab_mode = CursorGrabMode::Confined;
-  // }
+  let mut window = windows.single_mut();
+  if mouse.just_pressed(MouseButton::Left) {
+    window.cursor.visible = false;
+    window.cursor.grab_mode = CursorGrabMode::Confined;
+    cursor_state.set(CursorState::Locked);
+  }
 
-  // if key.just_pressed(KeyCode::Escape) {
-  //   window.cursor.visible = true;
-  //   window.cursor.grab_mode = CursorGrabMode::None;
-  // }
+  if key.just_pressed(KeyCode::Escape) {
+    window.cursor.visible = true;
+    window.cursor.grab_mode = CursorGrabMode::None;
+    cursor_state.set(CursorState::None);
+  }
 }
