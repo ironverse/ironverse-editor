7cff1c3b5172227840f0366be4bcfc7dd4bd5af6
ChaoticGood <colete_nico@yahoo.com>
2023-06-22 11:44:21 +0800

    Initial setup to fix conditional compilation to make iteration faster

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,133 @@
+#[cfg(feature = "minimum")]
 use bevy::{prelude::*, window::PresentMode};
+use bevy_flycam::NoCameraPlayerPlugin;
 
+#[cfg(feature = "tests")]
+mod tests;
 
-cfg_if::cfg_if! {
-  if #[cfg(not(feature = "tests"))] {
-    use bevy_flycam::NoCameraAndGrabPlugin;
+fn main() {
+  let mut app = App::new();
+  app
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+      primary_window: Some(Window {
+        title: "Ironverse Editor".into(),
+        resolution: (800., 600.).into(),
+        present_mode: PresentMode::AutoVsync,
+        fit_canvas_to_parent: true,
+        prevent_default_event_handling: false,
+        ..default()
+      }),
+      ..default()
+    }))
+    .add_plugin(NoCameraPlayerPlugin);
 
-    mod physics;
-    mod data;
-    mod states;
-    mod components;
-    mod graphics;
-    mod input;
-    mod utils;
-    mod debugger;
-    mod ui;
+  #[cfg(feature = "tests")]
+  app
+    .add_plugin(tests::ChunkPlugin);
 
-    #[cfg(target_arch = "wasm32")]
-    mod wasm;
-
-    #[cfg(feature = "minimalgraphics")]
-    mod minimalgraphics;
-
-    #[cfg(feature = "normalgraphics")]
-    mod normalgraphics;
-
-    /*
-      Able to modularized the features
-
-
-      Notes:
-        Debugger text and Egui are connected
-        Create a common plugins?
-        Disabling graphics module makes compilation time between ~12s to ~6s
-        Disabling Egui and ui compilation time between ~6s to 3s?
-        Use config file when necessary
-    */
-
-    fn main() {
-      let mut app = App::new();
-      app
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-          primary_window: Some(Window {
-            title: "Ironverse Editor".into(),
-            resolution: (800., 600.).into(),
-            present_mode: PresentMode::AutoVsync,
-            fit_canvas_to_parent: true,
-            prevent_default_event_handling: false,
-            ..default()
-          }),
-          ..default()
-        }))
-        .add_plugin(NoCameraAndGrabPlugin)
-        .add_plugin(physics::CustomPlugin)
-        .add_plugin(data::CustomPlugin)
-        .add_plugin(states::CustomPlugin)
-        .add_plugin(components::CustomPlugin)
-        .add_plugin(input::CustomPlugin)
-        .add_plugin(graphics::CustomPlugin)
-        // .add_plugin(debugger::CustomPlugin)
-        ;
-
-      #[cfg(feature = "gui_none")]
-      app
-        .add_plugin(ui::NonePlugin);
-
-      #[cfg(feature = "gui_normal")]
-      app
-        .add_plugin(ui::CustomPlugin);
-
-
-      #[cfg(feature = "minimalgraphics")]
-      app
-        .add_plugin(minimalgraphics::CustomPlugin);
-      
-
-      #[cfg(feature = "normalgraphics")]
-      app
-        .add_plugin(normalgraphics::CustomPlugin);
-
-      
-      #[cfg(target_arch = "wasm32")]
-      app
-        .add_plugin(wasm::CustomPlugin);
-
-      #[cfg(not(target_arch = "wasm32"))]
-      app
-        .add_plugin(native::CustomPlugin);
-
-      app.run();
-    }
-
-    // Comment out to make rust-analyzer work when compiling on linux/native
-    #[cfg(not(target_arch = "wasm32"))]
-    mod native;
-  }
+  app.run();
 }
 
 
+
+
+/* 
+use bevy::{prelude::*, window::PresentMode};
+use bevy_flycam::NoCameraAndGrabPlugin;
+
+mod physics;
+mod data;
+mod states;
+mod components;
+mod graphics;
+mod input;
+mod utils;
+mod debugger;
+mod ui;
+
+#[cfg(target_arch = "wasm32")]
+mod wasm;
+
+#[cfg(feature = "minimalgraphics")]
+mod minimalgraphics;
+
+#[cfg(feature = "normalgraphics")]
+mod normalgraphics;
+
+/*
+  Able to modularized the features
+
+
+  Notes:
+    Debugger text and Egui are connected
+    Create a common plugins?
+    Disabling graphics module makes compilation time between ~12s to ~6s
+    Disabling Egui and ui compilation time between ~6s to 3s?
+    Use config file when necessary
+*/
+
+fn main() {
+  let mut app = App::new();
+  app
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+      primary_window: Some(Window {
+        title: "Ironverse Editor".into(),
+        resolution: (800., 600.).into(),
+        present_mode: PresentMode::AutoVsync,
+        fit_canvas_to_parent: true,
+        prevent_default_event_handling: false,
+        ..default()
+      }),
+      ..default()
+    }))
+    .add_plugin(NoCameraAndGrabPlugin)
+    .add_plugin(physics::CustomPlugin)
+    .add_plugin(data::CustomPlugin)
+    .add_plugin(states::CustomPlugin)
+    .add_plugin(components::CustomPlugin)
+    .add_plugin(input::CustomPlugin)
+    .add_plugin(graphics::CustomPlugin)
+    // .add_plugin(debugger::CustomPlugin)
+    ;
+
+  #[cfg(feature = "gui_none")]
+  app
+    .add_plugin(ui::NonePlugin);
+
+  #[cfg(feature = "gui_normal")]
+  app
+    .add_plugin(ui::CustomPlugin);
+
+
+  #[cfg(feature = "minimalgraphics")]
+  app
+    .add_plugin(minimalgraphics::CustomPlugin);
+  
+
+  #[cfg(feature = "normalgraphics")]
+  app
+    .add_plugin(normalgraphics::CustomPlugin);
+
+  
+  #[cfg(target_arch = "wasm32")]
+  app
+    .add_plugin(wasm::CustomPlugin);
+
+  #[cfg(not(target_arch = "wasm32"))]
+  app
+    .add_plugin(native::CustomPlugin);
+
+  app.run();
+}
+
+// Comment out to make rust-analyzer work when compiling on linux/native
+#[cfg(not(target_arch = "wasm32"))]
+mod native;
+ */
+
+
+
+/* 
 cfg_if::cfg_if! {
   if #[cfg(feature = "tests")] {
     mod tests;
@@ -127,4 +158,4 @@ cfg_if::cfg_if! {
     }
   }
 }
-
+ */
--- src/tests/chunk.rs
+++ src/tests/chunk.rs
@@ -1,34 +1,90 @@
-use bevy::prelude::*;
-use voxels::data::voxel_octree::{VoxelOctree, ParentValueType};
+use bevy::{prelude::*, render::{render_resource::{PrimitiveTopology, VertexFormat}, mesh::{Indices, MeshVertexAttribute}}};
+use bevy_flycam::FlyCam;
+use voxels::{data::{voxel_octree::{VoxelOctree, ParentValueType, VoxelMode}, surface_nets::VoxelReuse}, utils::key_to_world_coord_f32, chunk::chunk_manager::ChunkManager};
 
 pub struct CustomPlugin;
 impl Plugin for CustomPlugin {
   fn build(&self, app: &mut App) {
     app
+      .add_startup_system(setup_camera)
       .add_startup_system(startup);
   }
 }
 
-fn startup() {
-  let start = 0;
-  let end = 16;
+fn setup_camera(
+  mut commands: Commands,
+) {
+  commands
+    .spawn(Camera3dBundle {
+      transform: Transform::from_xyz(0.0, 2.0, -5.0).looking_to(Vec3::Z, Vec3::Y),
+      ..Default::default()
+    })
+    .insert(FlyCam);
+
+  commands.spawn(PointLightBundle {
+    point_light: PointLight {
+      intensity: 6000.0,
+      ..Default::default()
+    },
+    transform: Transform::from_xyz(0.0, 15.0, 0.0),
+    ..Default::default()
+  });
+}
+
+fn startup(
+  mut commands: Commands,
+  mut meshes: ResMut<Assets<Mesh>>,
+  mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+  let start = 2;
+  let end = 14;
 
   let mut voxels = Vec::new();
   for x in start..end {
     for y in start..end {
       for z in start..end {
-        voxels.push([x as u32, y as u32, z as u32, 1 as u32]);
+
+        if y < 7 {
+          voxels.push([x as u32, y as u32, z as u32, 1 as u32]);
+        }
       }
     }
   }
-
 
   let mut octree = VoxelOctree::new_from_3d_array(
     0, 
     4, 
     &voxels, ParentValueType::DefaultValue
   );
+
+  let mut manager = ChunkManager::default();
+  let data = octree.compute_mesh2(VoxelMode::SurfaceNets, &mut manager.voxel_reuse);
+
+  let mut render_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+  render_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, data.positions.clone());
+  render_mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, data.normals.clone());
+  render_mesh.set_indices(Some(Indices::U32(data.indices.clone())));
+
+  render_mesh.insert_attribute(VOXEL_WEIGHT, data.weights.clone());
+  render_mesh.insert_attribute(VOXEL_TYPE_1, data.types_1.clone());
+
+  let mesh_handle = meshes.add(render_mesh);
+
+  let coord_f32 = key_to_world_coord_f32(&[0, 0, 0], manager.config.seamless_size);
+  commands
+    .spawn(MaterialMeshBundle {
+      mesh: mesh_handle,
+      material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+      transform: Transform::from_xyz(coord_f32[0], coord_f32[1], coord_f32[2]),
+      ..default()
+    });
 }
+
+pub const VOXEL_WEIGHT: MeshVertexAttribute =
+  MeshVertexAttribute::new("Voxel_Weight", 988540917, VertexFormat::Float32x4);
+
+pub const VOXEL_TYPE_1: MeshVertexAttribute =
+  MeshVertexAttribute::new("Voxel_Type_1", 988540918, VertexFormat::Uint32x4);
 
 
 
